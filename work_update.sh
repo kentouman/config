027850a5a7051e0b0b9921f5f05c0ef70bf0422b
kentouman <kentouman@gmail.com>
2015-02-23 09:31:41 -0800

    some fix

--- work_update.sh
+++ work_update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cp -r ./vim/.vim ~
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./vim/.vimrc ~
-cp ./zsh/.zshrc.work ~/.zshrc
+cp ./zsh/.zshrc_work ~/.zshrc
 cp ./zsh/.zhf_aliases ~
