ca82112771f3f3198f4c04b3b0146e4488b28923
kentouman <kentouman@gmail.com>
2015-02-23 09:22:35 -0800

    separate update script

--- updatevim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp -r ./vim/.vim ~
-cp ./vim/.vimrc ~
-cp ./zsh/.zshrc ~
-cp ./zsh/.zhf_aliases ~
---
+++ work_update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -r ./vim/.vim ~
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp ./vim/.vimrc ~
+cp ./zsh/.zshrc.work ~/.zshrc
+cp ./zsh/.zhf_aliases ~
